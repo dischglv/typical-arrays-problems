f40c07248b528df69c7546a059f1ca09d107fd8c
Diana Schegoleva <dschegoleva@gmail.com>
2021-03-05 14:06:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (args) {
+  if (!args || (args.length === 0)) return 0;
+
+  return args.sort(compareNumeric).shift();
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (args) {
+  if (!args || (args.length ===0)) return 0;
+
+  return args.sort(compareNumeric).pop();
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (args) {
+  if (!args || (args.length === 0)) return 0;
+
+  let sum = 0;
+  for (let arg of args) sum += arg;
+
+  return sum / args.length;
+}
+
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
 }
